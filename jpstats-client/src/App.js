931f3a94bbed74fd0ca480fd6586e4cc75267004
Mantas Maciulis <maciulispersonal@gmail.com>
2024-03-15 23:19:43 +0000

    chore(client): run prettier

--- jpstats-client/src/App.js
+++ jpstats-client/src/App.js
@@ -2,7 +2,7 @@ import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-<div className="App">
+    <div className='App'>
       <Navbar />
       {/* Other components will come here */}
     </div>
