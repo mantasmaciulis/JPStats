327030687d67a93b7150e60ed9ad1c259b29b497
Mantas Maciulis <maciulispersonal@gmail.com>
2024-03-16 21:03:40 +0000

    feat(dashboard): improve styling

--- jpstats-client/src/App.js
+++ jpstats-client/src/App.js
@@ -1,16 +1,15 @@
-// App.js
-
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Dashboard from './pages/Dashboard/Dashboard';
+import './App.css'
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        <Route path='/' element={<Dashboard className="dashboard" />} />
       </Routes>
     </div>
   );
