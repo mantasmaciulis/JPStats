625b2e4f18e6379dce5ed67cf86d48657062ee8f
Mantas Maciulis <maciulispersonal@gmail.com>
2024-03-16 21:22:21 +0000

    feat(dashboard): add JLPTCard

---
+++ jpstats-client/src/components/JLPTCard/JLPTCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './JLPTCard.css';
+import Card from '../Card/Card';
+
+function JLPTCard() {
+  return (
+    <Card className='progress-to-jlpt' title='Progress to JLPT N1'></Card>
+  );
+}
+
+export default JLPTCard;
--- jpstats-client/src/pages/Dashboard/Dashboard.js
+++ jpstats-client/src/pages/Dashboard/Dashboard.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Card from '../../components/Card/Card';
+import JLPTCard from '../../components/JLPTCard/JLPTCard';
 import './Dashboard.css';
 
 const Dashboard = () => {
   return (
     <div className='dashboard'>
       <Card className='recently-learned' title='Recently Learned Words'></Card>
-
-      <Card className='progress-to-jlpt' title='Progress to JLPT N1'></Card>
-
+      <JLPTCard></JLPTCard>
       <Card className='words-by-memory' title='Words By Memory Level'></Card>
 
       <Card className='grammar-progress' title='Grammar Progress'></Card>
